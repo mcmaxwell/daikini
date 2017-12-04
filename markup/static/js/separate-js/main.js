783ca3c6104538713fe68d7129a27dd9a48e1eb4
Maxim Liutsko <mcmaxwell07@gmail.com>
2017-12-04 08:49:27 +0200

    final up

--- markup/static/js/separate-js/main.js
+++ markup/static/js/separate-js/main.js
@@ -1,1 +1,14 @@
-var clock;$(document).ready(function(){var clock;clock=$('.clock').FlipClock({clockFace:'DailyCounter',autoStart:false,language:'ru',callbacks:{stop:function(){$('.message').html('The clock has stopped!')}}});clock.setTime(getSecondsToTomorrow());clock.setCountdown(true);clock.start();function getSecondsToTomorrow(){var now=new Date();var dateNeed=10;var dateNeedUse=new Date(now.getFullYear(),now.getMonth(),now.getDate()+dateNeed);var diff=dateNeedUse-now;return Math.round(diff/1000);}});
+var clock;$(document).ready(function(){
+  var clock;
+  clock=$('.clock').FlipClock({clockFace:'DailyCounter',autoStart:false,language:'ru',callbacks:{stop:function(){
+    $('.message').html('Время вышло')
+  }
+  }});clock.setTime(getSecondsToTomorrow());clock.setCountdown(true);clock.start();
+function getSecondsToTomorrow(){
+  var dt = "December 03 2017 14:22:00";
+  var first = new Date(dt);
+  var last = Date.now();
+
+  var diff=first-last;
+  return Math.round(diff/1000);
+}});
